c2e625844d768030f9e6682e67cd52fb86a2a314
SoledadAzu <solchisina@live.com.ar>
2022-01-25 23:57:55 -0300

    formulario

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static('public'));
 
 /*******Estableciendo mi Pagina HOME como inicio*** */
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'views','login.html')))
 
 /********Iniciando Servidor al final de todo****** */
 app.listen(port,()=> console.log("estoy en el puerto " + port))
